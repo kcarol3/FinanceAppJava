19670922fe0299a191ad9b9ba261ea57ab6962ec
Karol Kurowski <karol.123@vp.pl>
2025-03-27 08:31:44 +0100

    13, add flyweight design patterns

--- src/main/java/com/example/FinanceApp/controller/TransactionController.java
+++ src/main/java/com/example/FinanceApp/controller/TransactionController.java
@@ -3,20 +3,28 @@ package com.example.FinanceApp.controller;
 import com.example.FinanceApp.decorator.TransactionValidationException;
 import com.example.FinanceApp.dto.TransactionDTO;
 import com.example.FinanceApp.entity.base.Transaction;
+import com.example.FinanceApp.flyweight.transactionIcon.TransactionIconFactory;
 import com.example.FinanceApp.service.base.TransactionServiceInterface;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.text.ParseException;
 
 @RestController
 @RequestMapping("/transactions")
 public class TransactionController {
     private final TransactionServiceInterface transactionService;
+    private final TransactionIconFactory transactionIconFactory;
 
-    public TransactionController(TransactionServiceInterface transactionService) {
+    public TransactionController(TransactionServiceInterface transactionService, TransactionIconFactory transactionIconFactory) {
         this.transactionService = transactionService;
+        this.transactionIconFactory = transactionIconFactory;
     }
 
     @PostMapping
@@ -34,5 +42,21 @@ public class TransactionController {
     @PostMapping("/{transactionId}")
     public Transaction createRecurringTransaction(@PathVariable Long transactionId, @RequestParam String frequency) {
         return transactionService.createRecurringTransaction(transactionId, frequency);
+    }
+
+    @GetMapping("/{type}/image")
+    public ResponseEntity<byte[]> getTransactionImage(@PathVariable String type) throws IOException {
+        BufferedImage image = transactionIconFactory.getTransactionIcon(type.toUpperCase()).getImage();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_PNG_VALUE)
+                .body(convertImageToByteArray(image));
+    }
+
+    private byte[] convertImageToByteArray(BufferedImage image) throws IOException {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+            javax.imageio.ImageIO.write(image, "PNG", byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
+        }
     }
 }
